e61984705ee1d6935031321cf5d21c1a112a3c9a
kk-gc <kk.gc.lp@gmail.com>
2023-09-12 11:12:05 +0100

    Move user input validation from view to controller

--- controller.py
+++ controller.py
@@ -6,27 +6,27 @@ class SudokuController:
 
     def __init__(self):
         self._view = SudokuView()
-        self.raw_user_input = self._view.user_input()
+        self.user_input = self._view.get_user_input()
         self.user_input_ok = self.user_input_validate()
 
         self.sudoku = self.sudoku_make()
 
     def user_input_validate(self):
         # user_input should be 9 x 9 = 81 characters long
-        if len(self.raw_user_input) != 81:
+        if len(self.user_input) != 81:
             return False
 
         try:
             # user_input should be a string with numbers and spaces only
             # so after removing spaces should be able to convert to integer
-            int(self.raw_user_input.replace(' ', ''))
+            int(self.user_input.replace(' ', ''))
         except ValueError:
             return False
         return True
 
     def sudoku_make(self):
-        if self._view.user_input_valid:
-            return SudokuData(self._view.user_input)
+        if self.user_input_ok:
+            return SudokuData(self.user_input)
         return None
 
     # def sudoku_solve(self):
--- view.py
+++ view.py
@@ -1,24 +1,7 @@
 class SudokuView:
 
-    def __init__(self):
-        self.user_input = self.get_user_input()
-        self.user_input_valid = self.user_input_validate()
-
     def get_user_input(self):
         return input('sudoku: ')
-
-    def user_input_validate(self):
-        # user_input should be 9 x 9 = 81 characters long
-        if len(self.user_input) != 81:
-            return False
-
-        try:
-            # user_input should be a string with numbers and spaces only
-            # so after removing spaces should be able to convert to integer
-            int(self.user_input.replace(' ', ''))
-        except ValueError:
-            return False
-        return True
 
     @staticmethod
     def print_as_board(board):
