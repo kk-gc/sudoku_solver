55158078f5b8327e4f30fd3566b5cdd927323067
kk-gc <kk.gc.lp@gmail.com>
2023-10-12 18:03:42 +0100

    Added tests: all keys are strings, all values are ndarray

--- test/test_thresholding.py
+++ test/test_thresholding.py
@@ -2,6 +2,9 @@
 # relative path modules - known issue
 import sys
 import os
+
+import numpy as np
+
 sys.path.insert(0, os.getcwd())
 
 import pytest
@@ -13,6 +16,18 @@ def test_thresholding_image_good():
     image = cv2.imread('./sudoku.png')
     t = thresholding.thresholding(image, show_results=False)
     assert isinstance(t, dict)
+
+
+def test_thresholding_image_good_all_keys_are_strings():
+    image = cv2.imread('./sudoku.png')
+    t = thresholding.thresholding(image, show_results=False)
+    assert all([isinstance(key, str) for key in t.keys()])
+
+
+def test_thresholding_image_good_all_values_are_ndarray():
+    image = cv2.imread('./sudoku.png')
+    t = thresholding.thresholding(image, show_results=False)
+    assert all([isinstance(value, np.ndarray) for value in t.values()])
 
 
 def test_thresholding_image_good_results_bad():
