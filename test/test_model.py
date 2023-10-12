35ad89b221a7e834d33f048d76b02ce081afab56
kk-gc <kk.gc.lp@gmail.com>
2023-10-12 20:25:54 +0100

    Added test: string to board

--- test/test_model.py
+++ test/test_model.py
@@ -53,3 +53,19 @@ def test_sudoku_data_validate_raw_strings_bad(raw_string, is_valid):
 def test_sudoku_data_empties_to_zeroes(raw_string_valid, raw_string_zeroed):
     sz = model.SudokuData(raw_string_valid).string_zeroed
     assert sz == raw_string_zeroed
+
+
+@pytest.mark.parametrize('raw_string_valid',
+                         ['2 9  7      386972     9    6 75     53   86    6 85 4 9    4   2  65 19  79  6 3',
+                          '3      14 17346 894598 7 268 5 73    72     8   2  4     7 19 3 9  248  5       7',
+                          '913   42  8   4  525 1 96 3      5 8   658  2 3  9 7 4 4 9       6         4 7  6',
+                          '7    6  4  9       2   381 312    97     4 26   2      4 5      9 3   78   1  2  ',
+                          '  4  1  8        73  4     1  2 6  9    387   2     1  8 3   2  6  1     7     65',
+                          '2  8   7  9     3 1    49 8       9 4   5     1 3  6 2        7 6 2  8 1  2  3   ',
+                          ])
+def test_sudoku_data_string_to_board(raw_string_valid):
+    board = model.SudokuData(raw_string_valid).board
+    assert isinstance(board, list)
+    assert len(board) == 9
+    assert all([len(row) == 9 for row in board])    # all rows length = 9 ?
+    assert all([all([isinstance(number, int) for number in row]) for row in board])     # all rows contain only integers
